9f56b988c9f8f7ebcbf4593c39104d5e175eed93
Spawn <alkspawn@gmail.com>
2019-07-01 17:25:22 +0300

    final

--- src/test/java/it/alex/lab11/CalculatorAppTest.java
+++ src/test/java/it/alex/lab11/CalculatorAppTest.java
@@ -2,7 +2,40 @@ package it.alex.lab11;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorAppTest {
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        CalculatorApp calcApp = new CalculatorApp();
+        ConverterRPN calcRPN = new ConverterRPN();
+        ReadyInput readyInp = new ReadyInput();
+        ExpressionCalculator expressionCalc = new ExpressionCalculator();
+
+        String input = "( (4-9) *  2+15  ) /2-9*(2+1  )";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String[] expectedInput = {"(", "(", "4", "-", "9", ")", "*", "2", "+", "15", ")", "/", "2", "-", "9", "*", "(", "2", "+", "1", ")"};
+        String[] actualInput = readyInp.readyInput();
+        assertArrayEquals("Wrong input", expectedInput, actualInput);
+        String expectedRPN = "4 9 - 2 * 15 + 2 / 9 2 1 + * - ";
+        Field field = CalculatorApp.class.getDeclaredField("priorityMap");
+        field.setAccessible(true);
+        Map<String, Integer> map = (Map<String, Integer>) field.get(calcApp);
+        String actualRPN = calcRPN.getExpression(actualInput, map);
+        assertEquals("Wrong convert RPN", expectedRPN, actualRPN);
+        String expectedResult = "-24.5";
+        String actualResult = expressionCalc.expressionEvaluation(actualRPN);
+        assertEquals("Error", expectedResult,actualResult);
+
+    }
 
     //((4-9)*2+15)/2-9*(2+1)
     //-24.5
