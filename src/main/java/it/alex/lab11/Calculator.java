cff2e49e86f734e1640f765438575f7c1937aa0b
Spawn <alkspawn@gmail.com>
2019-07-20 17:25:22 +0300

    final

--- src/main/java/it/alex/lab11/Calculator.java
+++ src/main/java/it/alex/lab11/Calculator.java
@@ -29,7 +29,8 @@ public class Calculator {
     private String[] convertInputToArray(String string) {
         string = string.replaceAll("\\s", "");
         String operators = "(\\(|\\)|\\+|-|\\*|/)";
-        return string.split(String.format("(?<=%s)|(?=%s)", operators, operators));
+        String[] array = string.split(String.format("(?<=%s)|(?=%s)", operators, operators));
+        return array;
     }
 
     private Double getOperate(String symbol, String firstNumber, String secondNumber) {
@@ -82,10 +83,13 @@ public class Calculator {
     private String getReversePolishNotation(String[] input, Map<String, Integer> operationMap) {
         Stack<String> operatorStack = new Stack<>(); //Стек для хранения операторов
         String output = "";
+        int countNumber = 0;
+        int countOperate = 0;
         for (int i = 0; i < input.length; i++) {
             //Если число
             if (isNumber(input[i])) {
                 output += input[i] + " ";
+                countNumber++;
 
                 //Если оператор
             } else if (operationMap.containsKey(input[i])) {
@@ -98,6 +102,7 @@ public class Calculator {
                         symbol = operatorStack.pop();
                     }
                 } else {
+                    countOperate++;
                     if (!operatorStack.empty() && operationMap.get(input[i]) <= operationMap.get(operatorStack.peek())) {
                         output += operatorStack.pop() + " ";
                         operatorStack.push(input[i]);
@@ -110,17 +115,38 @@ public class Calculator {
         while (!operatorStack.empty()) {
             output += operatorStack.pop() + " ";
         }
+        System.out.println(countNumber);
+        System.out.println(countOperate);
         return output;
+    }
+
+    private void checkExpression(String[] array) {
+        boolean flag = false;
+        for (int i = 0; i < array.length; i++) {
+            if ((isNumber(array[i])) && flag == false) {
+                flag = true;
+            } else if (("+-/*".contains(array[i])) && flag == true) {
+                flag = false;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+        if (flag != true) {
+            throw new RuntimeException();
+        }
     }
 
     public double calculate(String expression) throws ExceptionCalculator {
         try {
             String[] array = convertInputToArray(expression);
+            checkExpression(array);
             String expressionRPN = getReversePolishNotation(array, priorityMap);
             return Double.valueOf(expressionEvaluation(expressionRPN));
-        } catch (RuntimeException e){
+        } catch (RuntimeException e) {
             throw new ExceptionCalculator(e);
         }
     }
+
 
 }
