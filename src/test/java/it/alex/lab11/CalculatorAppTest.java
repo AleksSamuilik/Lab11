5ede39960f1ec6e36d13565d66d9ef9c2afaf083
Spawn <alkspawn@gmail.com>
2019-07-27 08:31:16 +0300

    final

--- src/test/java/it/alex/lab11/CalculatorAppTest.java
+++ src/test/java/it/alex/lab11/CalculatorAppTest.java
@@ -1,7 +1,8 @@
 package it.alex.lab11;
 
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -14,44 +15,53 @@ public class CalculatorAppTest {
     @Parameterized.Parameters(name = "{index}:Expression{0}={1}")
     public static Iterable<Object[]> data() {
         Object[][] data = new Object[][]{
-                {"1+1*(3-2)", 2},
-                {"2+2*2", 6},
+                {"1+1*(3-2)", 2d},
+                {"2+2*2", 6d},
                 {"((4-   9)  *2+15   )/  2  - 9*(2      + 1)", -24.5},
-                {"( 2  +  2 *  2) * ( 9 / 3 * 2 - 3) / 2", 9},
-                {"(  (  (  ( 4 - 2 )*3 )/ 2) +5 *9 /3 +2 )/ 2", 10},
-                {"5+  9  *2  -( 8+ 4 ) * 9/     3 ", -13},
-                {"1+1(1)", 0},
-                {"((4-   9)  *2+15   )/  2  - 9*(2    a  + 1)", 0},
-                {"((4-   9)  *2+15   )/  2  - 9*(2    (  + 1)", 0},
-                {"((4-   9)  *2+15   )/  2  - 9*(2    *  + 1)", 0},
-                {"1+1*5+2*(5+)", 0},
-                {"(2*3)+((2-1)/2", 0},
-                {"5+2*", 0},
-                {"(4-2)+7)/2", 0},
-                {"dsa+sad-ssd", 0}
+                {"( 2  +  2 *  2) * ( 9 / 3 * 2 - 3) / 2", 9d},
+                {"(  (  (  ( 4 - 2 )*3 )/ 2) +5 *9 /3 +2 )/ 2", 10d},
+                {"5+  9  *2  -( 8+ 4 ) * 9/     3 ", -13d},
+                {"1+1(1)", "RuntimeException"},
+                {"((4-   9)  *2+15   )/  2  - 9*(2    a  + 1)", "RuntimeException"},
+                {"((4-   9)  *2+15   )/  2  - 9*(2    (  + 1)", "EmptyStackException"},
+                {"((4-   9)  *2+15   )/  2  - 9*(2    *  + 1)", "RuntimeException"},
+                {"1+1*5+2*(5+)", "RuntimeException"},
+                {"(2*3)+((2-1)/2", "EmptyStackException"},
+                {"5+2*", "RuntimeException"},
+                {"(4-2)+7)/2", "EmptyStackException"},
+                {"dsa+sad-ssd", "RuntimeException"}
         };
         return Arrays.asList(data);
     }
 
+
     private String expression;
     private double expected;
+    private String exception;
 
-    public CalculatorAppTest(String expression, double expected) {
+    public CalculatorAppTest(String expression, Object expected) {
         this.expression = expression;
-        this.expected = expected;
+        if (expected instanceof String) {
+            this.exception = (String) expected;
+        } else {
+            this.expected = (Double) expected;
+        }
     }
 
     Calculator calculator = new Calculator();
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
     @Test
     public void calculationTest() {
-        try {
+        if (null == exception) {
             assertEquals(expected, calculator.calculate(expression), 0.0);
-        } catch (Exception e) {
-            if (e.getCause() instanceof RuntimeException) {
-            } else {
-                Assert.fail("unexpected exception");
-            }
+        } else {
+            thrown.expect(ExceptionCalculator.class);
+            thrown.expectMessage(exception);
+            calculator.calculate(expression);
         }
     }
 }
